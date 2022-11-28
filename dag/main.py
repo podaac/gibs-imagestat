d364a5b07d00d90754a53419e94000f3ada383e1
vaishnaviyathirajam <31413898+vaishnaviyathirajam@users.noreply.github.com>
2022-11-28 16:48:31 -0500

    Refactor to remove subprocess calls (#12)
    * Add files via upload
    
    Got rid of the sub-processes
    
    * Ran some formatting and cleaned up some unused variables. Also had to change the BeautifulSoup filter on the "colormapentry" tag to ignore case in order to get it to work for the GHRSST_L4_MUR_Sea_Surface_Temperature layer. Also updated docker entrypoint and dependencies in poetry.
    
    Co-authored-by: Frank Greguska <Francis.Greguska@jpl.nasa.gov>

--- dag/main.py
+++ dag/main.py
@@ -1,5 +1,4 @@
 import uvicorn
 
 if __name__ == "__main__":
-    # uvicorn.run("api:app", host="ec2-3-65-18-201.eu-central-1.compute.amazonaws.com", port=8080, reload=True)
-    uvicorn.run("api:app", host="localhost", port=9000, reload=True)
+    uvicorn.run("api:app", host="127.0.0.1", port=9000, reload=True)
